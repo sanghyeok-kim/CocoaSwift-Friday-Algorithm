d5b64cf917ce504551c97774a8fcd6fea276fdcb
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-05 12:24:19 +0900

    p2438

---
+++ Friday-algorithm-Week1/Friday-algorithm-Week1/p2438.swift
@@ -0,0 +1,23 @@
+//
+//  p2438.swift
+//  Friday-algorithm-Week1
+//
+//  Created by 김상혁 on 2021/11/05.
+//
+
+import Foundation
+
+func p2438() {
+    let input = readLine() ?? ""
+    
+    guard let num: Int = Int(input) else {
+        return
+    }
+    
+    for i in 0..<num {
+        for _ in 0..<i {
+            print("*", terminator: "")
+        }
+        print("")
+    }
+}
