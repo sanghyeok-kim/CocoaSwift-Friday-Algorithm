dd5d95fcb010827be7d3b6fc8a7df62ddae3a79c
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-12 10:57:09 +0900

    Complete 12928-약수의합.swift

--- Friday-algorithm-Week2/Friday-algorithm-Week2/12928-약수의합.swift
+++ Friday-algorithm-Week2/Friday-algorithm-Week2/12928-약수의합.swift
@@ -5,3 +5,17 @@
 //  Created by 김상혁 on 2021/11/12.
 //
 
+func solution(_ n: Int) -> Int {
+    var sum = 0
+    
+    if n == 0 {
+        return 0
+    }
+    
+    for i in 1...n {
+        if n % i == 0 {
+            sum += i
+        }
+    }
+    return sum
+}
