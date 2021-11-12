7cd43af27a5d764eebe12f41064c28474039c0ee
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-12 15:03:49 +0900

    Complete 12901.swift

--- Friday-algorithm-Week2/Friday-algorithm-Week2/12901.swift
+++ Friday-algorithm-Week2/Friday-algorithm-Week2/12901.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+func solution(_ a:Int, _ b:Int) -> String {
+    
+    let Weeks = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+    let MonthDays = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    var daysfromNewYear = 0
+
+    for i in 0..<a {
+        daysfromNewYear += MonthDays[i]
+    }
+
+    let result = Weeks[(b + daysfromNewYear) % 7]
+    
+    return result
+}
