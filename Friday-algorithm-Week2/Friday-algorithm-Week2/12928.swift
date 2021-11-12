02cd9d39dc0875d88d3f3ec8b4d3f7617dd6d5f8
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-12 11:20:07 +0900

    Complete 12928.swift

--- Friday-algorithm-Week2/Friday-algorithm-Week2/12928.swift
+++ Friday-algorithm-Week2/Friday-algorithm-Week2/12928.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+func solution(_ n: Int) -> Int {
+    var sum = 0
+    
+    if n == 0 {
+        return 0
+    }
+    
+    for i in 1...n {
+        if n % i == 0 {
+            sum += i
+        }
+    }
+    return sum
+}
