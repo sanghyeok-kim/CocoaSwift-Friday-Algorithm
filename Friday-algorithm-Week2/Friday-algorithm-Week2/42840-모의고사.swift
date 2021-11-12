d5d4ecae91a324c912568dbfe4cc7cbe68586d4a
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-12 22:02:47 +0900

    Refactor - 단일 for문 3개 -> 2중 for문 1개

--- Friday-algorithm-Week2/Friday-algorithm-Week2/42840-모의고사.swift
+++ Friday-algorithm-Week2/Friday-algorithm-Week2/42840-모의고사.swift
@@ -13,26 +13,18 @@ func solution(_ answers:[Int]) -> [Int] {
     let wayOfTwo = [2, 1, 2, 3, 2, 4, 2, 5]
     let wayOfThr = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
+    let ways = [wayOfOne, wayOfTwo, wayOfThr]
+    
     var resultArr = [Int]()
     var counts = [0, 0, 0]
 
-    
-    for i in 0..<answers.count {
-        if answers[i] == wayOfOne[i % wayOfOne.count] {
-            counts[0] += 1
+    for i in 0..<ways.count {
+        for j in 0..<answers.count {
+            if answers[j] == ways[i][j % ways[i].count] {
+                counts[i] += 1
+            }
         }
     }
-    for i in 0..<answers.count {
-        if answers[i] == wayOfTwo[i % wayOfTwo.count] {
-            counts[1] += 1
-        }
-    }
-    for i in 0..<answers.count {
-        if answers[i] == wayOfThr[i % wayOfThr.count] {
-            counts[2] += 1
-        }
-    }
-    
     
     for i in 0..<counts.count {
         if counts[i] == counts.max() {
