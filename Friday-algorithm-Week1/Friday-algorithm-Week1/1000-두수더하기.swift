9374c01d58880506187b6dd1ed0f57313fe6af12
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-12 17:52:50 +0900

    이름 형식 변경

---
+++ Friday-algorithm-Week1/Friday-algorithm-Week1/1000-두수더하기.swift
@@ -0,0 +1,15 @@
+//
+//  p1000.swift
+//  Friday-algorithm-Week1
+//
+//  Created by 김상혁 on 2021/11/05.
+//
+
+func p1000() {
+    let input = readLine() ?? ""
+    let strArr = input.split(separator: " ")
+    let intArr: Array<Int> = strArr.map{Int($0)!}
+    let result = intArr[0] + intArr[1]
+    
+    print(result)
+}
