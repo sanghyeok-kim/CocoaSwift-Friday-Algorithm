239911daa50fd6f0948f7358ee60e429ee797fc8
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-12 13:55:40 +0900

    Complete 12910.swift

--- Friday-algorithm-Week2/Friday-algorithm-Week2/12910.swift
+++ Friday-algorithm-Week2/Friday-algorithm-Week2/12910.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
+    
+    var result = [Int]()
+    
+    for i in 0..<arr.count {
+        if arr[i] % divisor == 0 {
+            result.append(arr[i])
+        }
+    }
+    
+    if result.count == 0 {
+        result.append(-1)
+        return result
+    }
+    else {
+        return result.sorted()
+    }
+    
+}
