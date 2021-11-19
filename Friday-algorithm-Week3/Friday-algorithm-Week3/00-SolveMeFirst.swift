e25b5ac0e1371bd5b4be9147f58769686beea094
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-19 12:06:48 +0900

    Refactor 00-SolveMeFirst.swift - 주석추가

--- Friday-algorithm-Week3/Friday-algorithm-Week3/00-SolveMeFirst.swift
+++ Friday-algorithm-Week3/Friday-algorithm-Week3/00-SolveMeFirst.swift
@@ -5,6 +5,9 @@
 //  Created by 김상혁 on 2021/11/19.
 //
 
+//함수를 통해 튜플방식으로 input을 return하는 방식을 써보고 싶어서 약간 다르게 풀었음
+//(알고리즘 출제 페이지에 그대로 제출시 틀렸다고 나올 수 있음)
+
 func inputTwoNumbers() -> (Int, Int) {
     print("a = ", terminator: "")
     let inputA = Int(readLine()!)!
