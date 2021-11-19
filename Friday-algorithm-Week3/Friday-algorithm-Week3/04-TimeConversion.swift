bd1ca0b5c3e996441b85f3c05ac83e38090eedf1
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-19 15:24:01 +0900

    Complete 03-TimeConversion.swift

--- Friday-algorithm-Week3/Friday-algorithm-Week3/04-TimeConversion.swift
+++ Friday-algorithm-Week3/Friday-algorithm-Week3/04-TimeConversion.swift
@@ -11,6 +11,7 @@ func timeConversion(s: String) -> String {
     //.map { String($0) }해주는 이유: format12Time이 Array<SubString>이기때문
     var format12Time = s.dropLast(2).split(separator: ":").map{ String($0) }
     
+    
     if meridiem == "PM" && format12Time[0] != "12" {
             let convertedHour = Int(format12Time[0])! + 12
             format12Time[0] = String(convertedHour)
@@ -19,6 +20,7 @@ func timeConversion(s: String) -> String {
     else if meridiem == "AM" && format12Time[0] == "12" {
             format12Time[0] = "00"
     }
+    
     
     let result = format12Time.map { String($0) }.joined(separator: ":")
     return result
