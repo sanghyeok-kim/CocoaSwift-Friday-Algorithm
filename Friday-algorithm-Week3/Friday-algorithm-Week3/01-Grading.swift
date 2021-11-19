d9e1ecfe672c6c11e619d64bc21625687affe8b6
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-19 12:04:49 +0900

    Complete 01-Grading.swift

--- Friday-algorithm-Week3/Friday-algorithm-Week3/01-Grading.swift
+++ Friday-algorithm-Week3/Friday-algorithm-Week3/01-Grading.swift
@@ -5,4 +5,30 @@
 //  Created by 김상혁 on 2021/11/19.
 //
 
-import Foundation
+
+func inputScores() -> [Int] {
+    var scores = [Int]()
+
+    let studentsNumber = Int(readLine()!)!
+
+    for _ in 0..<studentsNumber {
+        let eachScore = Int(readLine()!)!
+        scores.append(eachScore)
+    }
+    return scores
+}
+
+
+func gradingStudents(grades: [Int]) {
+    var result = grades
+
+    for i in 0..<result.count {
+        
+        let gapOfNextMultiple5 = 5 - (result[i] % 5)
+        
+        if result[i] >= 38 && gapOfNextMultiple5 < 3 {
+            result[i] += gapOfNextMultiple5
+        }
+        print(result[i])
+    }
+}
