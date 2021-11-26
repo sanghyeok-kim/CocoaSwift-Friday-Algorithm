357b251cdc8da2a62faaed784fc8a48da2eb882f
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-26 18:14:21 +0900

    Complete 04-RomanToInteger

--- Friday-algorithm-Week4/Friday-algorithm-Week4/04-RomanToInteger.swift
+++ Friday-algorithm-Week4/Friday-algorithm-Week4/04-RomanToInteger.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+class Solution {
+    let dict: [Character: Int] = ["I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000]
+    
+    func romanToInt(_ s: String) -> Int {
+        var totalNumber = 0
+        var prevNumber = 0
+        
+        s.reversed().forEach {
+            let currentNumber = dict[$0] ?? 0
+            
+            if currentNumber >= prevNumber {
+                totalNumber += currentNumber
+            } else {
+                totalNumber -= currentNumber
+            }
+            prevNumber = currentNumber
+        }
+        return totalNumber
+    }
+}
