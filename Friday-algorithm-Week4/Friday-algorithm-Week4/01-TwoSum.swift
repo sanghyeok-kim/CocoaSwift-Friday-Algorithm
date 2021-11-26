a2662deb664a55fc8b73e85892a1459016694209
sanghyeok-kim <slsnsi1212@gmail.com>
2021-11-26 13:39:26 +0900

    Complete 01-TwoSum by O(N)

--- Friday-algorithm-Week4/Friday-algorithm-Week4/01-TwoSum.swift
+++ Friday-algorithm-Week4/Friday-algorithm-Week4/01-TwoSum.swift
@@ -10,6 +10,7 @@ import Foundation
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         
+        /*
         for i in 0..<nums.count {
             for j in (i + 1)..<nums.count {
                 if (nums[i] + nums[j]) == target {
@@ -18,7 +19,20 @@ class Solution {
                 }
             }
         }
+        */
         
+        var complements = [Int:Int]()
+
+        for i in 0..<nums.count {
+            if let targetIdx = complements[target - nums[i]] {
+
+                print("\(targetIdx), \(i)")
+                return [targetIdx, i]
+            }
+            else {
+                complements[nums[i]] = i
+            }
+        }
         return []
     }
 }
